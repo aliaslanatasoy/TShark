69c29721359ac5d21e82ecbecab46a0fb4a1bc97
Ali Aslan Atasoy <aslan.atasoy@etiya.com>
2017-06-09 17:03:17 +0300

    application.yml added

--- src/main/java/backend/controller/TestCaseController.java
+++ src/main/java/backend/controller/TestCaseController.java
@@ -1,10 +1,22 @@
 package backend.controller;
 
+import backend.service.TestCaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
 
 @RestController
 @RequestMapping(value = "/test/case")
 public class TestCaseController {
+    @Autowired
+    TestCaseService testCaseService;
 
+    @RequestMapping(value = "/list",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getAllTestCases(){
+        return null;
+    }
 }
