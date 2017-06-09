8b322a2254ee1d8085741068f98f214a6597f9d7
Ali Aslan Atasoy <aslan.atasoy@etiya.com>
2017-06-09 17:36:52 +0300

    application.yml added

--- src/main/java/backend/controller/TestCaseController.java
+++ src/main/java/backend/controller/TestCaseController.java
@@ -3,6 +3,7 @@ package backend.controller;
 import backend.service.TestCaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
